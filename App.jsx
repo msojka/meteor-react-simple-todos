6ef63d067a384134c7e23580719cb9e29418271b
Martin Sojka <sojka.martin@gmail.com>
2015-10-30 15:01:42 +0100

    Add complete tasks hiding and incomplete count.

--- App.jsx
+++ App.jsx
@@ -2,9 +2,20 @@ App = React.createClass({
   
   mixins: [ReactMeteorData],
   
-  getMeteorData() {
+  getInitialState() {
     return {
-      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+      hideCompleted: false
+    }
+  },
+  
+  getMeteorData() {
+    let query = {};
+    if(this.state.hideCompleted) {
+      query = {checked: {$ne: true}}
+    }
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count()
     }
   },
   
@@ -24,11 +35,25 @@ App = React.createClass({
     React.findDOMNode(this.refs.textInput).value = '';
   },
   
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  },
+  
   render() {
     return(
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.data.incompleteCount})</h1>
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
           <form className="new-task" onSubmit={this.handleSubmit}>
             <input type="text" ref="textInput" placeholder="Type to add new tasks" />
           </form>
