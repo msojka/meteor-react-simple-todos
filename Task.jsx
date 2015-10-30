638a092704a7c9e20f3b3aa585f43fca539224ae
Martin Sojka <sojka.martin@gmail.com>
2015-10-30 14:33:19 +0100

    Add checkbox and delete to task.

--- Task.jsx
+++ Task.jsx
@@ -4,9 +4,26 @@ Task = React.createClass({
     task: React.PropTypes.object.isRequired
   },
   
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: {checked: !this.props.task.checked}
+    });
+  },
+  
+  delete() {
+    Tasks.remove(this.props.task._id);
+  },
+  
   render() {
+    const taskClassName = this.props.task.checked ? "checked" : "";
     return(
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.delete}>
+          &times;
+        </button>
+        <input type="checkbox" readOnly={true} checked={this.props.task.checked} onClick={this.toggleChecked} />
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
   
